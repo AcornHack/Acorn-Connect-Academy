bdc869c53ded7857f939170dfb090fdf91d09eda
Arun Pattni <arun.pattni01@gmail.com>
2017-07-26 12:18:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,4 +2,4 @@
  * Created by Arun on 18/07/2017.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://acorn:acorn@ds033956.mlab.com:33956/acorn'); //message me if you want the URI.
+mongoose.connect('URI'); //message me if you want the URI.
