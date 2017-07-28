29f6168c8be7012e6ca093cb941701d130281584
Arun Pattni <arun.pattni01@gmail.com>
2017-07-28 13:51:17 +0100

    Fix for issue #3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var app = express();
+
 var db = require('./db');
 
 var UserController = require('./project/ProjectController');
--- db.js
+++ db.js
@@ -2,4 +2,5 @@
  * Created by Arun on 18/07/2017.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://acorn:acorn@ds033956.mlab.com:33956/acorn'); //message me if you want the URI.
+var dburi = process.env._DATABASE_URI;
+mongoose.connect(dburi);
